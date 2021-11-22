9cc778eaaf79facee9000a52024c055663dca0ab
Derek Banister <ptownderek@yahoo.com>
2021-11-21 18:37:08 -0800

    projects component added

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import React from 'react';
 import Navbar from './components/Nav';
 import About from './components/About';
 import Techs from './components/Techs';
-// import Projects from './components/Projects';
+import Projects from './components/Projects';
+import 'bootstrap/dist/css/bootstrap.min.css';
 // import Footer from './components/Footer';
 
 function App() {
     return (
-        <div className="">
+        <div>
             <Navbar />
             <About />
             <Techs />
-            {/* <Projects />
-            <Footer /> */}
+            <br />
+            <Projects />
+            {/* <Footer />  */}
         </div>
     );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,3 +1,47 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/style.css';
+
+const styles = {
+    cardDeck: {
+        margin: "10px",
+        display: "flex",
+        justifyContent: "center"
+    }
+}
+
+function Projects() {
+    return (
+        <div class="card-deck" style={styles.cardDeck}>
+            <div class="card">
+                <div class="card-body">
+                    <h2 class="card-title">TV Remote</h2>
+                    <a class="card-text" href="https://github.com/DerekBanister/tvremote" target="_blank">
+                        Github
+                        Repo</a>
+                    {/* add project link here */}
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h2 class="card-title">Tech Blog</h2>
+                    <a class="card-text" href="https://github.com/DerekBanister/MVC-tech-blog" target="_blank">
+                        Github
+                        Repo</a>
+                    {/* add project link here */}
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <h2 class="card-title">Dungeon Runner</h2>
+                    <a class="card-text" href="https://github.com/DerekBanister/fullstack-browser-game" target="_blank">
+                        Github
+                        Repo</a>
+                    {/* add project link here */}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Projects;
