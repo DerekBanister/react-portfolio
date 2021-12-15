39314d6c8b0e8eaf3d3a37d3900c806e9423c1cf
Derek Banister <ptownderek@yahoo.com>
2021-12-15 15:37:16 -0800

    troubleshooting resume

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,21 +13,11 @@ function Footer() {
             <a href="https://github.com/DerekBanister" target="_blank" rel="noreferrer" class="icon"> <GrGithub /> </a>
             {/* <a href="../images/DereksResume.pdf" target="_blank" class="button email">Download My Resume</a> */}
 
-            <a href="../../public/resume/resume.pdf" class="button email" download>Download My Resume</a>
+            <a href="../../public/resume.pdf" class="button email" download>Download My Resume</a>
         </div>
     )
 
 }
-
-
-
-
-
-
-
-
-
-
 
 
 export default Footer;
