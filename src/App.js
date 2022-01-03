84468bf1042f12b2dcd5a028ec1e9aee0b915ccd
Derek Banister <ptownderek@yahoo.com>
2022-01-03 13:00:41 -0800

    trying to fix resume download issue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 //import here
 import React from 'react';
 import Container from "./components/Container";
-// import Footer from './components/Footer';
 
 const App = () => <Container />;
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import resume from '../images/resume.pdf';
 import '../styles/style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { GrLinkedin } from 'react-icons/gr';
 import { GrGithub } from 'react-icons/gr';
-// npm install react-icons < DiJavascript />
 
 function Footer() {
+
     return (
         <div class="text-center footer text-dark" >
             <a href="https://www.linkedin.com/in/derek-banister/" target="_blank" rel="noreferrer" class="icon">< GrLinkedin /></a>
             <a href="https://github.com/DerekBanister" target="_blank" rel="noreferrer" class="icon"> <GrGithub /> </a>
-            <a className="button email" href={resume} download="Derek's Resume">Download Resume</a>
+            <a className="button email" href="resume.pdf" download>Download Resume</a>
         </div>
     )
 
